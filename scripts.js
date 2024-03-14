8ea446acaeaecbe2f8222a42da12b9d6d1f5960c
FANNY PRETRE <fanny.pretre@gmail.com>
2024-03-14 20:06:23 +0100

    Design des modales

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 /* En cours de test */
 const cards = document.querySelector(".cards");
 const modalCards = document.querySelector(".modal-cards");
+const modal2Cards = document.querySelector(".modal-2-cards");
 
 const worksofArt = [
     {
@@ -9,70 +10,79 @@ const worksofArt = [
         label1: "#PopArt",
         label2: "#Culture",
         backgroundImage: "assets/oeuvre/free-photo-of-lunettes-de-soleil-art-creatif-festival.jpeg",
+        texte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat et erat id malesuada. Maecenas scelerisque neque arcu, ac luctus sapien hendrerit sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin sed hendrerit lacus. Nullam tempor lacinia sapien auctor ullamcorper. Cras accumsan, sem et sodales vulputate, tellus mauris lobortis est, nec feugiat orci erat eu justo. Aliquam vitae accumsan purus. Morbi tortor nisl, consequat luctus purus id, congue posuere nisl. Praesent sit amet augue pellentesque, consectetur tellus ut, mollis dolor. Etiam dictum quam nec massa mattis, id porttitor urna aliquam. Mauris finibus, nisi vitae dapibus viverra, arcu neque cursus dolor, a faucibus purus ante eu mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed elementum lorem eget massa sollicitudin ultricies et id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras vitae bibendum tortor, quis sagittis ex. Sed id justo vel libero dapibus volutpat vel ut dui. ",
 
     },
 
     {
         artistName: "Artiste 2",
         artName: "Art 2",
-        label1: "PopArt",
-        label2: "Culture",
+        label1: "#PopArt",
+        label2: "#Culture",
         backgroundImage: "assets/oeuvre/street-art-shipping-container-freight-highway-163811.jpeg",
+        texte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat et erat id malesuada. Maecenas scelerisque neque arcu, ac luctus sapien hendrerit sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin sed hendrerit lacus. Nullam tempor lacinia sapien auctor ullamcorper. Cras accumsan, sem et sodales vulputate, tellus mauris lobortis est, nec feugiat orci erat eu justo. Aliquam vitae accumsan purus. Morbi tortor nisl, consequat luctus purus id, congue posuere nisl. Praesent sit amet augue pellentesque, consectetur tellus ut, mollis dolor. Etiam dictum quam nec massa mattis, id porttitor urna aliquam. Mauris finibus, nisi vitae dapibus viverra, arcu neque cursus dolor, a faucibus purus ante eu mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed elementum lorem eget massa sollicitudin ultricies et id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras vitae bibendum tortor, quis sagittis ex. Sed id justo vel libero dapibus volutpat vel ut dui. ",
     },
 
     {
         artistName: "Artiste 3",
         artName: "Art 3",
-        label1: "PopArt",
-        label2: "Culture",
+        label1: "#PopArt",
+        label2: "#Culture",
         backgroundImage: "assets/oeuvre/pexels-photo-1194420.jpeg",
+        texte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat et erat id malesuada. Maecenas scelerisque neque arcu, ac luctus sapien hendrerit sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin sed hendrerit lacus. Nullam tempor lacinia sapien auctor ullamcorper. Cras accumsan, sem et sodales vulputate, tellus mauris lobortis est, nec feugiat orci erat eu justo. Aliquam vitae accumsan purus. Morbi tortor nisl, consequat luctus purus id, congue posuere nisl. Praesent sit amet augue pellentesque, consectetur tellus ut, mollis dolor. Etiam dictum quam nec massa mattis, id porttitor urna aliquam. Mauris finibus, nisi vitae dapibus viverra, arcu neque cursus dolor, a faucibus purus ante eu mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed elementum lorem eget massa sollicitudin ultricies et id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras vitae bibendum tortor, quis sagittis ex. Sed id justo vel libero dapibus volutpat vel ut dui. ",
     },
 
     {
         artistName: "Artiste 4",
         artName: "Art 4",
-        label1: "PopArt",
-        label2: "Culture",
+        label1: "#PopArt",
+        label2: "#Culture",
         backgroundImage: "assets/oeuvre/pexels-photo-1194420.jpeg",
+        texte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat et erat id malesuada. Maecenas scelerisque neque arcu, ac luctus sapien hendrerit sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin sed hendrerit lacus. Nullam tempor lacinia sapien auctor ullamcorper. Cras accumsan, sem et sodales vulputate, tellus mauris lobortis est, nec feugiat orci erat eu justo. Aliquam vitae accumsan purus. Morbi tortor nisl, consequat luctus purus id, congue posuere nisl. Praesent sit amet augue pellentesque, consectetur tellus ut, mollis dolor. Etiam dictum quam nec massa mattis, id porttitor urna aliquam. Mauris finibus, nisi vitae dapibus viverra, arcu neque cursus dolor, a faucibus purus ante eu mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed elementum lorem eget massa sollicitudin ultricies et id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras vitae bibendum tortor, quis sagittis ex. Sed id justo vel libero dapibus volutpat vel ut dui. ",
     },
 
     {
         artistName: "Artiste 5",
         artName: "Art 5",
-        label1: "PopArt",
-        label2: "Culture",
+        label1: "#PopArt",
+        label2: "#Culture",
         backgroundImage: "assets/oeuvre/musee-art-contemporain-mac-paola-pivi.jpeg",
+        texte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat et erat id malesuada. Maecenas scelerisque neque arcu, ac luctus sapien hendrerit sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin sed hendrerit lacus. Nullam tempor lacinia sapien auctor ullamcorper. Cras accumsan, sem et sodales vulputate, tellus mauris lobortis est, nec feugiat orci erat eu justo. Aliquam vitae accumsan purus. Morbi tortor nisl, consequat luctus purus id, congue posuere nisl. Praesent sit amet augue pellentesque, consectetur tellus ut, mollis dolor. Etiam dictum quam nec massa mattis, id porttitor urna aliquam. Mauris finibus, nisi vitae dapibus viverra, arcu neque cursus dolor, a faucibus purus ante eu mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed elementum lorem eget massa sollicitudin ultricies et id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras vitae bibendum tortor, quis sagittis ex. Sed id justo vel libero dapibus volutpat vel ut dui. ",
     },
 
     {
         artistName: "Artiste 6",
         artName: "Art 6",
-        label1: "PopArt",
-        label2: "Culture",
+        label1: "#PopArt",
+        label2: "#Culture",
         backgroundImage: "assets/oeuvre/jeff-koons-ballerina-sculpture-3.jpg",
+        texte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat et erat id malesuada. Maecenas scelerisque neque arcu, ac luctus sapien hendrerit sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin sed hendrerit lacus. Nullam tempor lacinia sapien auctor ullamcorper. Cras accumsan, sem et sodales vulputate, tellus mauris lobortis est, nec feugiat orci erat eu justo. Aliquam vitae accumsan purus. Morbi tortor nisl, consequat luctus purus id, congue posuere nisl. Praesent sit amet augue pellentesque, consectetur tellus ut, mollis dolor. Etiam dictum quam nec massa mattis, id porttitor urna aliquam. Mauris finibus, nisi vitae dapibus viverra, arcu neque cursus dolor, a faucibus purus ante eu mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed elementum lorem eget massa sollicitudin ultricies et id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras vitae bibendum tortor, quis sagittis ex. Sed id justo vel libero dapibus volutpat vel ut dui. ",
     },
 
     {
         artistName: "Artiste 7",
         artName: "Art 7",
-        label1: "PopArt",
-        label2: "Culture",
+        label1: "#PopArt",
+        label2: "#Culture",
         backgroundImage: "assets/oeuvre/Ben-Turnbull-Supermen-Eleven-yatzer-4.jpg",
+        texte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat et erat id malesuada. Maecenas scelerisque neque arcu, ac luctus sapien hendrerit sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin sed hendrerit lacus. Nullam tempor lacinia sapien auctor ullamcorper. Cras accumsan, sem et sodales vulputate, tellus mauris lobortis est, nec feugiat orci erat eu justo. Aliquam vitae accumsan purus. Morbi tortor nisl, consequat luctus purus id, congue posuere nisl. Praesent sit amet augue pellentesque, consectetur tellus ut, mollis dolor. Etiam dictum quam nec massa mattis, id porttitor urna aliquam. Mauris finibus, nisi vitae dapibus viverra, arcu neque cursus dolor, a faucibus purus ante eu mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed elementum lorem eget massa sollicitudin ultricies et id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras vitae bibendum tortor, quis sagittis ex. Sed id justo vel libero dapibus volutpat vel ut dui. ",
     },
 
     {
         artistName: "Artiste 8",
         artName: "Art 8",
-        label1: "PopArt",
-        label2: "Culture",
+        label1: "#PopArt",
+        label2: "#Culture",
         backgroundImage: "assets/oeuvre/PixArt.png",
+        texte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat et erat id malesuada. Maecenas scelerisque neque arcu, ac luctus sapien hendrerit sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin sed hendrerit lacus. Nullam tempor lacinia sapien auctor ullamcorper. Cras accumsan, sem et sodales vulputate, tellus mauris lobortis est, nec feugiat orci erat eu justo. Aliquam vitae accumsan purus. Morbi tortor nisl, consequat luctus purus id, congue posuere nisl. Praesent sit amet augue pellentesque, consectetur tellus ut, mollis dolor. Etiam dictum quam nec massa mattis, id porttitor urna aliquam. Mauris finibus, nisi vitae dapibus viverra, arcu neque cursus dolor, a faucibus purus ante eu mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed elementum lorem eget massa sollicitudin ultricies et id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras vitae bibendum tortor, quis sagittis ex. Sed id justo vel libero dapibus volutpat vel ut dui. ",
     },
     {
         artistName: "Artiste 9",
         artName: "Art 9",
-        label1: "Space",
-        label2: "StreetArt",
+        label1: "#Space",
+        label2: "#StreetArt",
         backgroundImage: "assets/oeuvre/Test.jpg",
+        texte: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat et erat id malesuada. Maecenas scelerisque neque arcu, ac luctus sapien hendrerit sed. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin sed hendrerit lacus. Nullam tempor lacinia sapien auctor ullamcorper. Cras accumsan, sem et sodales vulputate, tellus mauris lobortis est, nec feugiat orci erat eu justo. Aliquam vitae accumsan purus. Morbi tortor nisl, consequat luctus purus id, congue posuere nisl. Praesent sit amet augue pellentesque, consectetur tellus ut, mollis dolor. Etiam dictum quam nec massa mattis, id porttitor urna aliquam. Mauris finibus, nisi vitae dapibus viverra, arcu neque cursus dolor, a faucibus purus ante eu mi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed elementum lorem eget massa sollicitudin ultricies et id justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras vitae bibendum tortor, quis sagittis ex. Sed id justo vel libero dapibus volutpat vel ut dui. ",
     },
 ];
 
@@ -179,6 +189,12 @@ function createModalCard(workModal) {
     modalClose.classList.add("modal-close");
     modalCard.appendChild(modalClose);
 
+    //create Closebtn
+    let closeBtn = new Image();
+    closeBtn.src = 'assets/icons/fermer.png';
+    closeBtn.classList.add("close");
+    closeBtn.alt = "Croix";
+    modalClose.appendChild(closeBtn)
 
     //create Header
     const modalHeader = document.createElement("div");
@@ -233,9 +249,11 @@ function createModalCard(workModal) {
     modalLabel2.innerText = `${workModal.label2}`;
     modalLabel.appendChild(modalLabel2);
 
-    modalClose.addEventListener("click", () => closeModal())
+    closeBtn.addEventListener("click", () => closeModal())
+    modalImg.addEventListener("click", () => openModal2(workModal))
 }
 
+// Pour clear la modal  
 function clearModal() {
     const modalOverlay = document.querySelector('.overlay');
     if (modalOverlay) {
@@ -249,11 +267,70 @@ function openModal(work) {
     clearModal();
     createModalCard(work);
     document.querySelector('.overlay').style.display = 'block';
-    document.querySelector('.modal-card').classList.add('modal--open')
+    document.querySelector('.modal-card').classList.add('modal-open')
     document.querySelector('.sectionFooter').style.display = 'none';
 }
 
 function closeModal() {
     clearModal();
+    document.querySelector('.sectionFooter').style.display = 'block';
 }
 
+
+
+
+// Pour la modal 2 
+// Data modal 2
+
+
+function createModal2(work2Modal) {
+
+    const modal2Overlay = document.createElement("div");
+    modal2Overlay.classList.add("overlay");
+    modal2Cards.appendChild(modal2Overlay);
+
+    const modal2Card = document.createElement("div");
+    modal2Card.classList.add("modal-card");
+    modal2Overlay.appendChild(modal2Card);
+
+    const modal2Close = document.createElement("div");
+    modal2Close.classList.add("modal-close");
+    modal2Card.appendChild(modal2Close);
+
+    //create Closebtn
+    let closeBtn = new Image();
+    closeBtn.src = 'assets/icons/fermer.png';
+    closeBtn.classList.add("close");
+    closeBtn.alt = "Croix";
+    modal2Close.appendChild(closeBtn)
+
+    //create body
+
+    const modal2Body = document.createElement("div");
+    modal2Body.classList.add("modal-body");
+    modal2Card.appendChild(modal2Body);
+
+
+    //create Image
+    const modal2Img = document.createElement("img");
+    modal2Img.src = work2Modal.backgroundImage;
+    modal2Img.classList.add("modal-img-1");
+    modal2Body.appendChild(modal2Img);
+
+    //create Closebtn
+    const closeBtn2 = document.createElement("button")
+    closeBtn2.innerText = "Retourner à l'accueil";
+    closeBtn2.classList.add("homepageBtn");
+    modal2Body.appendChild(closeBtn2)
+
+    closeBtn.addEventListener("click", () => closeModal())
+    closeBtn2.addEventListener("click", () => closeModal())
+}
+
+function openModal2(work2Modal) {
+    clearModal();
+    createModal2(work2Modal);
+    document.querySelector('.overlay').style.display = 'block';
+    document.querySelector('.modal-card').classList.add('modal-open')
+    document.querySelector('.sectionFooter').style.display = 'none';
+}
